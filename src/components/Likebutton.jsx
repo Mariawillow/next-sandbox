943bdc5aac1125d01c5873d92a465de5fa8cad82
TroelsAtWork <tras@kea.dk>
2025-03-25 12:12:17 +0100

    add likebutton

--- src/components/Likebutton.jsx
+++ src/components/Likebutton.jsx
@@ -5,9 +5,10 @@ import { useState } from "react";
 const LikeButton = (props) => {
   const [likes, setLikes] = useState(0);
   return (
-    <div>
-      <div></div>
-    </div>
+    <button onClick={() => setLikes(likes + 1)}>
+      <FaRegThumbsUp /> {props.label}
+      {likes > 1 ? "s" : ""} {likes}
+    </button>
   );
 };
 
