ef12522b57c4bc8298f6531a89ad8f27d35169e9
Mariawillow <mani0013@stud.kea.dk>
2025-03-27 13:02:41 +0100

    r

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,20 @@
 // import LikeButton from "@/components/Likebutton";
-// import Cookiecard from "@/components/Cookiecard";
- import { RxCookie } from "react-icons/rx";
+import Cookiecard from "@/components/Cookiecard";
+//  import { RxCookie } from "react-icons/rx";
 
 
 //sfc -> snippet til at oprette kompomnent syntax
 export default function Home() {
   return (
+    <div><Cookiecard /></div>
+
     
-<div class="cookie-popup" id="popup" popover>
+/* <div class="cookie-popup" id="popup" popover>
 <RxCookie />
 <h2>Im a popup</h2>
         <p>We use cookies to improve your user experience.</p>
         <button id="acceptButton">I Like Cookies</button>
-    </div>
+    </div> */
 
     // <div>
     //   {/* <LikeButton label="I Like Cookies" /> */}
--- src/components/Cookiecard.jsx
+++ src/components/Cookiecard.jsx
@@ -1,6 +1,24 @@
 "use client";
-// import { RxCookie } from "react-icons/rx";
+import { RxCookie } from "react-icons/rx";
 // import { useState } from "react";
+
+//jsx er elementer x strå for extention, noget det læggen ovenpå js, som gør det mulgit for os at skrive html i js.
+    
+const cookiePopup = () => {
+    return(
+        <section className="cookie-consent-popup">
+            <header className="cookie-consent-popup-header">
+                <RxCookie size={50} />
+            </header>
+            <footer className="cookie-consent-popup-footer">
+                <p className="cookie-consent-popup-paragraph"> bla bla </p>
+                <button className="cookie-consent-popup-button"> I Like Cookies </button>
+            </footer>
+        </section>
+    )
+}
+
+export default cookiePopup;
 
 
 
